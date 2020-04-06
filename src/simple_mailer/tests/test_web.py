94a048cb663b1c22f7f12dd501d3113bbec6ee65
Rigel <zedr@zedr.com>
2020-04-06 23:32:00 +0200

    Make the endpoint configurable
    I haven't found a good way to test this, so it's not tested by
    the suite for now.

--- src/simple_mailer/tests/test_web.py
+++ src/simple_mailer/tests/test_web.py
@@ -5,11 +5,15 @@ from webtest import TestApp
 from simple_mailer.web import get_application
 
 
-def test_post_mail_app(smtpd):
+def _set_test_environ(smtpd):
     os.environ.setdefault("SMTP_PORT", str(smtpd.port))
     os.environ.setdefault("USE_TLS", "false")
     os.environ.setdefault("FROM_ADDRESS", "me@example.com")
     os.environ.setdefault("TO_ADDRESS", "you@example.com")
+
+
+def test_post_mail_app(smtpd):
+    _set_test_environ(smtpd)
     app = TestApp(get_application())
     response = app.post("/mail", {})
     assert response.status_code == 200
--- src/simple_mailer/web.py
+++ src/simple_mailer/web.py
@@ -7,14 +7,15 @@ from simple_mailer.mailer import Mailer
 
 class Config:
     def __init__(self):
-        self.SMTP_HOST: str = os.environ.get("SMTP_HOST", "localhost")
-        self._SMTP_PORT: str = os.environ.get("SMTP_PORT", "465")
-        self.SMTP_PASSWD: str = os.environ.get("SMTP_PASSWD", "")
-        self.SMTP_USERID: str = os.environ.get("SMTP_USERID", "")
-        self.MAIL_TO: str = os.environ.get("TO_ADDRESS", "")
-        self.MAIL_FROM: str = os.environ.get("FROM_ADDRESS", "")
-        self.MAIL_SUBJECT: str = os.environ.get("MAIL_SUBJECT", "")
-        self._USE_TLS: str = os.environ.get("USE_TLS", "true")
+        self.SMTP_HOST: str = os.environ.get('SMTP_HOST', 'localhost')
+        self._SMTP_PORT: str = os.environ.get('SMTP_PORT', '465')
+        self.SMTP_PASSWD: str = os.environ.get('SMTP_PASSWD', '')
+        self.SMTP_USERID: str = os.environ.get('SMTP_USERID', '')
+        self.MAIL_TO: str = os.environ.get('TO_ADDRESS', '')
+        self.MAIL_FROM: str = os.environ.get('FROM_ADDRESS', '')
+        self.MAIL_SUBJECT: str = os.environ.get('MAIL_SUBJECT', '')
+        self._USE_TLS: str = os.environ.get('USE_TLS', 'true')
+        self.MAILER_PATH = os.environ.get('MAILER_PATH', '/mail')
 
     @property
     def SMTP_PORT(self) -> int:
@@ -28,7 +29,7 @@ class Config:
         return True if self._USE_TLS.lower() == "true" else False
 
 
-@bottle.post("/mail")
+@bottle.post(Config().MAILER_PATH)
 def mail():
     try:
         config = Config()
