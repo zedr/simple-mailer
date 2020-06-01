8d615498d5d35c4281046e42c48b34e991644522
Rigel <zedr@zedr.com>
2020-06-02 00:46:59 +0200

    Keep some newlines

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def long_description():
 
 setup(
     name='simple-mailer',
-    version='0.15.4',
+    version='0.15.5',
     author='Rigel Di Scala',
     author_email='zedr@zedr.com',
     description='A simple mailer for web forms',
